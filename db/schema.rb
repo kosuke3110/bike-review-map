e6db4692dacc25a14dd40afc7fa4eb37345389a5
斎藤晃輔 <saitokosuke@skMacBook-Air.local>
2025-06-24 03:50:39 +0900

    devise機能実装デフロイ

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_23_135253) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_23_184528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
