cc74ab20b790cc78d5bbed29fae36881c28b8aac
斎藤晃輔 <saitokosuke@skMacBook-Air.local>
2025-06-26 22:38:49 +0900

    レビュー削除編集機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_review, only: [:edit, :update, :destroy]
+    before_action :authorize_user!, only: [:edit, :update]
 
   def new
     @review = Review.new(shop_place_id: params[:place_id])
@@ -14,11 +16,37 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    # @review は before_action で取得済み
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to shop_path(@review.shop_place_id), notice: "レビューを更新しました！"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    shop_place_id = @review.shop_place_id
+    @review.destroy
+    redirect_to shop_path(shop_place_id), notice: "レビューを削除しました"
+  end
+
   private
 
   def review_params
     params.require(:review).permit(
       :shop_place_id, :rating_overall, :rating_skill, :rating_service, :rating_price, :comment
     )
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def authorize_user!
+    redirect_to root_path, alert: "権限がありません" unless @review.user == current_user
   end
 end
